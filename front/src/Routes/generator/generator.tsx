7908f87bbe374ac09845b2f72377bd68c8e9a344
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-11 08:49:43 +0100

    set up project front

---
+++ front/src/Routes/generator/generator.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function generator() {
+  return (
+    <div>generator</div>
+  )
+}
+
+export default generator
---
+++ front/src/Routes/toplist/toplist.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function toplist() {
+  return (
+    <div>toplist</div>
+  )
+}
+
+export default toplist
