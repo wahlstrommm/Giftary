dad3511dd8e56b1061e695dbfbc3ab9fd4061375
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-16 17:29:21 +0100

    Create a easy front page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="w-screen h-screen bg-salte-600">
+      
+      <Navbar/>
+      <div className="bg-red-600 w-full h-4/6 flex justify-center items-center">
+        <div className="h-4/5 bg-amber-900 w-8/12 flex justify-center items-center">
+          <div className="bg-amber-500 h-3/4 w-2/4 relative top-4 flex flex-col">
+            {/* img */}
+            <p>HEJ</p>
+            <div className="flex justify-center gap-9 relative bottom-2 items-center">
+              <Link to={"/Generator"}>
+              
+              <button className="bg-green-100 w-28">generator</button>
+              </Link>
+              <Link to={"/Login"}>
+                <button className="bg-green-700 w-28">Logga in</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <Footer />
+      </footer>
+
+    </main>
   );
 }
 
