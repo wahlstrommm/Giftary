960ac23ba5def612390a89573b87964219527cf4
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-16 18:02:59 +0100

    interface for product

---
+++ front/src/Models/Product/IProduct.tsx
@@ -0,0 +1,11 @@
+export interface IProduct {
+
+    name:string,
+    summary:string,
+    age:string,
+    aimedFor:string,
+    price:string,
+    image:any,
+    favorited:boolean,
+    category:string
+}
