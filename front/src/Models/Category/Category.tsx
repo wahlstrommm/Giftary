2cd02616d8d2a7225007038e6eb78d02ab26fc7c
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-16 18:02:10 +0100

    categoty class

---
+++ front/src/Models/Category/Category.tsx
@@ -0,0 +1,12 @@
+export class Category {
+    forHim: string;
+    forher: string;
+    forBoth: string;
+    clothes: string;
+   constructor(forHim:string, forher:string, forBoth:string, clothes:string) {
+(this.forHim = forHim);
+(this.forher = forher);
+(this.forBoth = forBoth);
+(this.clothes = clothes);
+}
+}
