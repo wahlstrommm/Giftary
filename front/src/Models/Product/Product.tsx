3923e331ab16f7097ed16e9af0e768f29ecc8b47
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-16 18:03:09 +0100

    class for product

---
+++ front/src/Models/Product/Product.tsx
@@ -0,0 +1,21 @@
+export class Product {
+    name:string;
+    summary:string;
+    age:string;
+    aimedFor:string;
+    price:string;
+    image:any;
+    favorited:boolean;
+    category:string;
+    constructor(name:string,  summary:string, age:string, aimedFor:string, price:string, image:any,favorited:boolean,category:string) {
+      (this.name = name);
+        (this.summary = summary);
+        (this.age = age);
+        (this.aimedFor = aimedFor);
+        (this.price = price);
+        (this.image = image);
+        (this.favorited=favorited);
+        (this.category=category);
+    }
+  }
+  
