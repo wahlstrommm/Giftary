74ab7207f4194a96f048331bff64313dcdc6ace5
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-16 22:04:21 +0100

    connect to mongodb

--- backend/app.js
+++ backend/app.js
@@ -7,14 +7,30 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const productRoute = require("./routes/product-route");
 var app = express();
-
+const PORT = 3001;
+const mongoose = require("mongoose");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+const dotenv = require("dotenv");
 
+dotenv.config();
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/api/products", productRoute);
+const uri = process.env.MONGODB_URI;
+
+async function init() {
+  try {
+    const options = { useNewUrlParser: true, useUnifiedTopology: true };
+    await mongoose.connect(uri, options);
+    console.log("connected to database");
+  } catch (error) {
+    console.log("error", error);
+  }
+  app.listen(PORT, () => console.log("lyssnar på port", PORT));
+}
+init();
 module.exports = app;
