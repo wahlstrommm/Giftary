9915abbfaba92779e99ed3037166e8dfae76491c
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-16 18:02:50 +0100

    class for new company

---
+++ front/src/Models/Company/NewCompany.tsx
@@ -0,0 +1,13 @@
+export class NewCompany {
+        name: string;
+        orgNumber: string;
+        products: any[];
+        password: any;
+       constructor(name:string, orgNumber:string, products:any[], password:any) {
+    (this.name = name);
+    (this.orgNumber = orgNumber);
+    (this.products = products);
+    (this.password = password);
+    }
+  }
+  
