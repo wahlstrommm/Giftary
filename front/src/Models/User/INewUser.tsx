5cb2defc48e53c11c9d0ad01db76c77af049faa7
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-16 18:03:20 +0100

    interface for new user

---
+++ front/src/Models/User/INewUser.tsx
@@ -0,0 +1,14 @@
+
+
+export interface INewUser{
+
+
+      firstName: any,
+      lastName: any,
+      sex: any,
+      phone: any,
+      email: any,
+      password: any,
+      // _id: any,
+      productList:any[]
+}
