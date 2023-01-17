0bc4885ec1b2eb356385660f98f83ef8ca9d4d3a
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-17 12:54:25 +0100

    login in comp is done

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,12 +1,15 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const bcrypt = require("bcrypt");
 const userModel = require("../Models/User-model");
 const companyModel = require("../Models/Company-model");
-const User = require("../models/User");
+
 //Loggar in användare. Decryptar lösen etc.
 router.post("/", async (req, res) => {
-  if ("firstName" in req.body) {
+  //checkar först om det är en "vanlig användaer" eller företag som loggar in.
+  let test = req.body;
+  console.log(test);
+  if ("email" in test) {
     let testUser;
     userModel.findOne({ email: req.body.email }, function (err, data) {
       testUser = data;
@@ -49,6 +52,47 @@ router.post("/", async (req, res) => {
       }
     });
   } else {
+    let testCompany;
+    companyModel.findOne(
+      { orgNumber: req.body.orgNumber },
+      function (err, data) {
+        testCompany = data;
+        if (testCompany === null) {
+          return res.send("finns ingen användare");
+        } else {
+          try {
+            bcrypt.compare(
+              req.body.password,
+              testCompany.password,
+              function (err, response) {
+                if (err) {
+                  console.log("fel" + err);
+                }
+                if (response) {
+                  let confirmLoggin = {
+                    isAllowed: response,
+                    _id: testCompany._id,
+                    name: testCompany.name,
+                    orgNumber: testCompany.orgNumber,
+                    products: testCompany.products,
+                  };
+                  res.json(confirmLoggin);
+                } else {
+                  res.send("Fail fel lösen");
+                  return console.log({
+                    success: false,
+                    message: "passwords do not match",
+                  });
+                }
+              }
+            );
+          } catch {
+            console.log(err);
+            res.setHeader(500).send(err, "Fel");
+          }
+        }
+      }
+    );
   }
 });
 
