da13bc98cfd6970fd8a416aca063147250cb1597
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-19 16:42:13 +0100

    fixed the backend with res message

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -7,17 +7,17 @@ router.get("/", async (req, res) => {
   res.json({ "Hello from GET endpoint": req.body });
 });
 router.post("/", async (req, res) => {
+  console.log(req.body);
   let userEmail = req.body.email;
   await userModel.findOne({ email: req.body.email }).then((data) => {
     userEmail = data;
     return userEmail;
   });
   if (userEmail) {
-    res
-      .status(400)
-      .send(
-        "Blev något fel testa igen! Det verkar vara som denna mail används redan"
-      );
+    res.status(400).json({
+      message:
+        "Blev något fel testa igen! Det verkar vara som denna mail används redan",
+    });
   } else {
     try {
       const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -33,13 +33,15 @@ router.post("/", async (req, res) => {
 
       try {
         const Newuser = await user.save();
-        res.json({ status: "OK" });
+        res.status(200).json({ message: "OK", Newuser });
+        return;
       } catch (error) {
         res.status(400).json({ message: error.message });
         console.log("error 400", error);
       }
     } catch (error) {
-      res.status(500).send();
+      console.log("eror", error);
+      res.status(500).send(error.message);
     }
   }
 });
