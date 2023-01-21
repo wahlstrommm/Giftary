77106ca4c69b0211b6757d99fa4a4fa9fa67d83f
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-21 23:32:16 +0100

    prettier

--- front/src/Components/Modal.tsx
+++ front/src/Components/Modal.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Modal = () => {
-    return (  <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
-    <div className="bg-white p-2 rounded w-72">
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
+      <div className="bg-white p-2 rounded w-72">
         <h1>HI</h1>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-}
- 
 export default Modal;
