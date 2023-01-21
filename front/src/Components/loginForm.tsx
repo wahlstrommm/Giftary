cab42abc268b588fc55c7dff021cab76ba30abed
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-21 23:33:09 +0100

    loginform layout

--- front/src/Components/loginForm.tsx
+++ front/src/Components/loginForm.tsx
@@ -1,11 +1,37 @@
-import React, { useState,ChangeEvent} from "react";
+import React, { useState } from "react";
 import CreateUserform from "./CreateUserform";
-const LoginForm = () => {
+// import { useForm } from "react-hook-form";
+import LoginCompany from "./LoginCompany";
+import LoginUser from "./LoginUser";
 
-  const  [userInfo,setUserInfo]=useState({
-    username:"",
-    password:""
-  })
+const LoginForm = () => {
+  // const { register, handleSubmit,reset , formState: { errors } } = useForm();
+  // const [userChoice, setUserChoice] = useState("");
+  const [loginContainerCompany, setLoginContainerCompany] = useState(false);
+  const [loginContainerUser, setLoginContainerUser] = useState(false);
+  // const  [userInfo,setUserInfo]=useState({
+  //   username:"",
+  //   password:""
+  // })
+
+  // const checkWhatTypeLogin = (user:string)=>{
+  //   return /^\d+$/.test(str)
+  // }
+  // const handleTypeOfLogin = (e:any|undefined)=>{
+  //   console.log(e.target.event);
+  //   setUserChoice(e);
+  // }
+  // const onSubmit = (data:any) => {
+  //   let email = data.email
+  //   console.log(data)
+  //   // if(/^[0-9]+$/.test(email)){
+  //   //   console.log("nummer");
+  //   // }else{
+  //   //   console.log("sträng");
+  //   // }
+
+  //   // console.log(errors);
+  // };
 
   const [showContainer, setShowContainer] = useState(false);
 
@@ -13,79 +39,64 @@ const LoginForm = () => {
     setShowContainer(!showContainer);
   };
 
-  const handleLogin =()=>{
-    console.log("USER OBJ",userInfo)
-  }
-
-  const handleUsername = (e:ChangeEvent<HTMLInputElement>) => {  
-    let username = e.target.value;
-    console.log("username",username);
-    setUserInfo({ ...userInfo, username:username})
-  };
-
-  const handlePassword = (e:ChangeEvent<HTMLInputElement>) => {
-    let password = e.target.value;
-    console.log("password",password);
-    setUserInfo({ ...userInfo, password:password})
-  };
-
   return (
     <div className="flex justify-center">
       <div className="w-full max-w-xs">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div className="mb-4">
-            <label
-              htmlFor="username"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              Användarnamn
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              placeholder="Användarnamn"
-              onChange={handleUsername}
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="password"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              Lösenord
-            </label>
-            <input
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="******************"
-              onChange={handlePassword}
-            />
-            <p className="text-red-500 text-xs italic">
-              Please choose a password.
-            </p>
-          </div>
-          <div className="flex items-center justify-between">
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-row justify-evenly">
+          <div className="flex justify-evenly gap-6">
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
-              onClick={()=>handleLogin()}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              name="privat"
+              value="privat"
+              onClick={() => setLoginContainerUser(!loginContainerUser)}
             >
-              Logga in
+              Privat
+            </button>
+            <button
+              type="button"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              name="company"
+              value="company"
+              onClick={() => setLoginContainerCompany(!loginContainerCompany)}
+            >
+              Företag
             </button>
           </div>
-        </form>
-        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        </div>
+        <div
+          style={
+            loginContainerUser === true
+              ? { display: "block" }
+              : { display: "none" }
+          }
+        >
+          <h1>ÄR i Privat</h1>
+          <LoginUser />
+        </div>
+        {/* className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-around items-center"  */}
+        <div
+          style={
+            loginContainerCompany === true
+              ? { display: "block" }
+              : { display: "none" }
+          }
+        >
+          {/* <h1>ÄR i Företag</h1> */}
+          <LoginCompany />
+        </div>
+
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 text-center">
           <button
             className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
             onClick={() => handleClick()}
           >
+            {" "}
             Skapa ett konto
           </button>
         </div>
         <div style={showContainer ? { display: "block" } : { display: "none" }}>
-          <CreateUserform></CreateUserform>
+          <CreateUserform />
         </div>
         <p className="text-center text-gray-500 text-xs">
           &copy;2023 MW Corp. All rights reserved.
