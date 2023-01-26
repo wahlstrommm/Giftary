05346704b4007b4b32ecc841963ce43c8220b837
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-26 10:31:43 +0100

    clean up and added if user is logged in

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import { Link } from "react-router-dom";
-
+import Icon from "./Images/image-icon.svg";
+import { underline } from "colors";
+//checks LocalStorage
+let LocalS: any;
+const checkLS = () => {
+  let LS: any = localStorage.getItem("loggedinUser");
+  let LSParsed = JSON.parse(LS);
+  if (LSParsed) {
+    console.log("finns");
+    if (LSParsed.isAllowed) {
+      console.log("finns och är allowed");
+      LocalS = LSParsed;
+      console.log(LSParsed);
+    } else {
+      console.log("den är nu:", "{}");
+    }
+  } else {
+    console.log("finns inget utan helt tom");
+    LocalS = {
+      isAllowed: false,
+    };
+  }
+};
 function App() {
+  const [checkUser, setCheckUser] = useState(false);
+
+  checkLS();
+
   return (
     <main className="w-screen h-screen bg-salte-600">
       <Navbar />
       <div className="bg-red-600 w-full h-4/6 flex justify-center items-center">
-        <div className="h-4/5 bg-amber-900 w-8/12 flex justify-center items-center">
-          <div className="bg-amber-500 h-3/4 w-2/4 relative top-4 flex flex-col">
-            {/* img */}
+        <div className="h-4/5 bg-amber-900 w-10/12 flex justify-center items-center">
+          <div className="bg-amber-500 h-5/6 w-9/12 relative top-1 flex flex-col justify-evenly text-center">
+            <img src={Icon} className="h-24" alt="icon" />
             <p>HEJ</p>
             <div className="flex justify-center gap-9 relative bottom-2 items-center">
               <Link to={"/Generator"}>
-                <button className="bg-green-100 w-28">generator</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline m-5">
+                  Generator
+                </button>
               </Link>
-              <Link to={"/Login"}>
-                <button className="bg-green-700 w-28">Logga in</button>
-              </Link>
+              <div
+                style={
+                  LocalS.isAllowed === false || undefined
+                    ? { display: "block" }
+                    : { display: "none" }
+                }
+              >
+                <Link to={"/Login"}>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline m-5">
+                    Logga in
+                  </button>
+                </Link>
+              </div>
+              <div
+                style={
+                  LocalS.isAllowed === true && LocalS.type === "user"
+                    ? { display: "block" }
+                    : { display: "none" }
+                }
+              >
+                <Link to={"/UserProductList"}>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline m-5">
+                    Dina listor
+                  </button>
+                </Link>
+              </div>
+              <div
+                style={
+                  LocalS.isAllowed === true && LocalS.type === "company"
+                    ? { display: "block" }
+                    : { display: "none" }
+                }
+              >
+                <Link to={"/CreateProduct"}>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline m-5">
+                    Skapa produkt
+                  </button>
+                </Link>
+              </div>
+              <div
+                style={
+                  LocalS.isAllowed === true && LocalS.type === "company"
+                    ? { display: "block" }
+                    : { display: "none" }
+                }
+              >
+                <Link to={"/ProductOverview"}>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline m-5">
+                    Alla dina produkter
+                  </button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
