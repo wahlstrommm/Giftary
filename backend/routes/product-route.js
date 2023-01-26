6fee126cd432533e03dea49207e94e59f945e2fc
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-26 10:30:14 +0100

    multer but  i think i will remove it later

--- backend/routes/product-route.js
+++ backend/routes/product-route.js
@@ -4,21 +4,38 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const productModel = require("../Models/Product-model");
 const UserModel = require("../Models/User-model");
-router.get("/", async (req, res) => {
+const multer = require("multer");
+// const upload = multer({ dest: "public/upload/" });
+// const storage = multer.diskStorage;
+// let storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "public/upload");
+//   },
+//   filename: function (req, res, cb) {
+//     cb(null, "test_");
+//   },
+// });
+// const upload = multer({ storage: storage });
+router.get("/:company", async (req, res) => {
+  console.log("HEJ", req.params.company, req.body);
+  console.log("HEJ2", req.body);
   CompanyModel.findOne(
-    { name: new RegExp("^" + req.body.companyName + "$", "i") },
+    { name: new RegExp("^" + req.params.company + "$", "i") },
     function (err, resp) {
       if (resp) {
         console.log(resp);
         res.status(200).json({ products: resp.products });
       } else {
-        res.status(400).json({ products: "Finns inga" });
+        res.status(400).json({ products: "Finns inga", test: resp });
       }
     }
   );
 });
 // ! IF OM DET INTE FINNS NÅGRA PRODUKTER
 router.post("/", async (req, res) => {
+  // console.warn(req.body.image);
+  // console.log("HEJ");
+  console.log(req.body);
   const product = await productModel.create(req.body);
   const companyName = req.body.companyName;
   console.log("Innan", companyName);
@@ -35,7 +52,11 @@ router.post("/", async (req, res) => {
       }
     }
   );
-  res.status(200).json(product);
+  res.status(200).json({
+    message: "Produkten är nu skapad!",
+    status: res.status,
+    product: product,
+  });
 });
 router.get("/sort", async (req, res) => {
   console.log(req.body.category);
@@ -54,15 +75,22 @@ router.put("/", async (req, res) => {
   res.send("Hello from PUT endpoint");
 });
 
-router.get("/:id", async (req, res) => {
-  console.log(req.body.id);
-  let productId = req.body.id;
+router.post("/details/:id", async (req, res) => {
+  let productId = req.params.id;
   let find = await productModel.find({
     _id: mongoose.Types.ObjectId(productId),
   });
-  console.log(find);
-
-  //res.rediect + id???
-  res.status(200).json(find);
+  console.log("FIND", find);
+  if (find) {
+    //had to change it to a string else i get the object
+    let productID = find[0]._id.toString();
+    //send back the product aswell as the url for the front to handle it.
+    res.status(200).json({
+      url: "http://localhost:3002/product/" + productID,
+      Foundproduct: find[0],
+    });
+  } else {
+    res.status(500).json({ message: "Fel" });
+  }
 });
 module.exports = router;
