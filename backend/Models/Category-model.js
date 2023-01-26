dfb362efec1b3f1217fde6317ce15b29f90f87ca
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-26 10:28:22 +0100

    clean up the schema i will adjust it later

--- backend/Models/Category-model.js
+++ backend/Models/Category-model.js
@@ -1,17 +1,9 @@
 const mongoose = require("mongoose");
 const { Schema, model, models } = require("mongoose");
 const categoryModel = mongoose.Schema({
-  forHim: {
-    type: Boolean | any,
-  },
-  forHer: {
-    type: Boolean | any,
-  },
-  forBoth: {
-    type: Boolean | any,
-  },
-  clothes: {
-    type: Boolean | any,
-  },
+  forHim: {},
+  forHer: {},
+  forBoth: {},
+  clothes: {},
 });
 module.exports = mongoose.model("Category", categoryModel);
