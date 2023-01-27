c15b02cb82d2b34d016d0c5661dc3818318d916b
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-27 08:18:10 +0100

    did the save the product to user part

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const mongoose = require("mongoose");
-const UserModel = require("../Models/User-model");
+const userModel = require("../Models/User-model");
 const productModel = require("../Models/Product-model");
 const CompanyModel = require("../Models/Company-model");
 
@@ -20,49 +20,35 @@ router.get("/", async (req, res) => {
   );
 });
 
-router.post("/", async (req, res) => {
-  // console.log(req.body.email);
-  // console.log(req.body._id);
+//For adde a product to thier list
+router.post("/:id", async (req, res) => {
+  //gets the product id from params
+  let productId = req.params.id;
+  console.log(req.params.id, "ID");
+  //finds it
+  let find = await productModel.findOne({
+    _id: mongoose.Types.ObjectId(productId),
+  });
 
-  let productId = req.body._id;
-  let user;
-  UserModel.findOne(
-    { email: new RegExp("^" + req.body.email + "$", "i") },
-    function (err, doc) {
-      console.log("i den", doc);
-      if (doc) {
-        user = doc;
-        //hämtar id
-        //func
+  console.log("FIND!!!!!", find.favorited);
+  //Then i set it to true for future feature of hard and soft delete
+  find.favorited = true;
+  console.log("FIND!!!!!", find.favorited);
 
-        productModel.findById(productId, function (err, result) {
-          if (result) {
-            //kollar så de inte redan har sparat den
-            result.favorited = true;
-            user.productList.push(result);
-            user.save();
-            // user.productList.forEach((i) => {
-            //   if (result._id.toString() == i._id.toString()) {
-            //     console.log("finns redan");
-            //     res.status(200).send({ result: "finns redan" });
-            //     return;
-            //   } else {
-            //     console.log(result._id.toString());
-
-            //     console.log("läggs till");
-            res.status(200).send({ result: "läggs till", user });
-          } else {
-            console.log("error : ", err);
-            res.status(400).send({ error: err });
-          }
-        });
-        // console.log("USer", user);
-      } else {
-        res.status(400).send({ error: err });
-        console.log("Finns inget");
-      }
+  console.log("FIND", find);
+  //Gets the user so i can add the item to thier list
+  userModel.findOne({ email: req.body.email }, function (err, doc) {
+    console.log("i den", doc);
+    if (doc) {
+      console.log(doc.productsList);
+      doc.productList.push(find);
+      doc.save();
+      res.send(doc);
+    } else {
+      console.log("Finns inget");
+      res.send("fel");
     }
-  );
+  });
 });
 
 module.exports = router;
