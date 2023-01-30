c382341e26a6391b50969640b772061e6fbc61c6
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-01-30 12:04:05 +0100

    fix so that the user cant acess login if loggedin

--- front/src/Pages/Login.tsx
+++ front/src/Pages/Login.tsx
@@ -1,7 +1,23 @@
 import LoginForm from "../Components/loginForm";
 import Navbar from "../Components/Navbar";
 
+const checkLS = async () => {
+  let LS: any = localStorage.getItem("loggedinUser");
+  let LSParsed = JSON.parse(LS);
+  console.log(LSParsed);
+  if (LSParsed) {
+    console.log("finns");
+    if (LSParsed.isAllowed && LSParsed.type === "company") {
+      window.location.href = "http://localhost:3002/";
+    } else if (LSParsed.isAllowed && LSParsed.type === "user") {
+      window.location.href = "http://localhost:3002/";
+    }
+  } else {
+    console.log("finns inget utan helt tom");
+  }
+};
 const Login = () => {
+  checkLS();
   return (
     <div>
       <Navbar />
