f12fefc93e6431f2b700f8ce154bc523f2a4b467
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-02-04 11:32:57 +0100

    changed a href to Link

--- front/src/Components/Navbar.tsx
+++ front/src/Components/Navbar.tsx
@@ -35,9 +35,9 @@ const Navbar = () => {
       <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <a href="javascript:void(0)">
+            <Link to="/">
               <h2 className="text-2xl font-bold">LOGO</h2>
-            </a>
+            </Link>
             <div className="md:hidden">
               <button
                 className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
