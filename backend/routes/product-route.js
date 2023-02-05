40df11d319c40abe240f135042c207fd068adb5e
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-02-06 00:11:00 +0100

    clean up comments

--- backend/routes/product-route.js
+++ backend/routes/product-route.js
@@ -3,12 +3,8 @@ const CompanyModel = require("../Models/Company-model");
 const router = express.Router();
 const mongoose = require("mongoose");
 const productModel = require("../Models/Product-model");
-// const UserModel = require("../Models/User-model");
-// const multer = require("multer");
 
 router.get("/:company", async (req, res) => {
-  // console.log("HEJ", req.params.company, req.body);
-  // console.log("HEJ2", req.body);
   CompanyModel.findOne(
     { name: new RegExp("^" + req.params.company + "$", "i") },
     function (err, resp) {
@@ -25,13 +21,11 @@ router.get("/:company", async (req, res) => {
 router.post("/", async (req, res) => {
   const product = await productModel.create(req.body);
   const companyName = req.body.companyName;
-  // console.log("Innan", companyName);
   CompanyModel.findOne(
     { name: new RegExp("^" + companyName + "$", "i") },
     function (err, doc) {
       console.log("i den", doc);
       if (doc) {
-        // console.log(doc.products);
         doc.products.push(product);
         doc.save();
       } else {
@@ -45,24 +39,11 @@ router.post("/", async (req, res) => {
     product: product,
   });
 });
-// router.get("/sort", async (req, res) => {
-//   console.log(req.body.category);
-//   let find = await productModel.find({
-//     category: req.body.category,
-//   });
-//   let result = await find;
-//   if (find.length > []) {
-//     res.status(200).json(result);
-//   } else {
-//     res.json({ "Finns inga": find });
-//   }
-// });
 
 router.get("/", async (req, res) => {
   productModel.find({}).then(function (products) {
     res.send(products);
   });
-  // res.send("Hello from PUT endpoint");
 });
 
 router.post("/details/:id", async (req, res) => {
