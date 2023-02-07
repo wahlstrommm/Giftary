107b95305b8914b63013abfa78a7804147f4cad2
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-02-07 23:25:30 +0100

    added some more on the 404 page

--- front/src/Pages/NorFound.tsx
+++ front/src/Pages/NorFound.tsx
@@ -1,10 +1,15 @@
 import Navbar from "../Components/Navbar";
-
+import mike from "../Images/MikeW.png";
 const NotFound = () => {
   return (
-    <div>
+    <div className="bg-gradient-to-t from-gray-700 via-gray-900 to-black w-screen h-screen ">
       <Navbar />
-      <h1>Not found</h1>
+      <div className="bg-white flex items-center justify-center h-full">
+        <div className="h-1/3 w-1/3">
+          <img src={mike} alt="Mike W" />
+          <h1>Grattis du har hittat något som du inte borde... </h1>
+        </div>
+      </div>
     </div>
   );
 };
