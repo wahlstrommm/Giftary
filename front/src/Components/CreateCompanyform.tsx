289882be7b233cef1323eb3d6e24452202141a01
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-02-07 14:38:42 +0100

    aria label

--- front/src/Components/CreateCompanyform.tsx
+++ front/src/Components/CreateCompanyform.tsx
@@ -3,20 +3,20 @@ import { useForm } from "react-hook-form";
 import { INewCompany } from "../Models/Company/INewCompany";
 
 const CreateCompanyform = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [reponsText, setReponsText] = useState("");
+  //react form
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
   } = useForm();
-  const [showModal, setShowModal] = useState(false);
-  const [reponsText, setReponsText] = useState("");
-
+  //popup modal
   const handleModal = (e: any | void | undefined | HTMLDivElement) => {
-    // console.log(e);
     setShowModal(!showModal);
   };
-
+  //submit
   const onSubmit = (data: any) => {
     if (data) {
       let company: INewCompany = {
@@ -79,6 +79,7 @@ const CreateCompanyform = () => {
           <input
             type="text"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-fslate-900 focus:shadow-outline"
+            aria-label="shorter name for the company"
             placeholder="Aktiekod "
             {...register("name", { required: true, max: -2, maxLength: 12 })}
           />
@@ -95,6 +96,7 @@ const CreateCompanyform = () => {
           <input
             type="text"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-fslate-900 focus:shadow-outline"
+            aria-label="org number input"
             placeholder="Organisationsnummer"
             {...register("orgNumber", { required: true, maxLength: 80 })}
           />
@@ -113,6 +115,7 @@ const CreateCompanyform = () => {
           <input
             type="text"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-fslate-900 focus:shadow-outline"
+            aria-label="password input"
             placeholder="Lösenord"
             {...register("password", { required: true, maxLength: 100 })}
           />
@@ -131,6 +134,7 @@ const CreateCompanyform = () => {
           <input
             type="text"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-fslate-900 focus:shadow-outline"
+            aria-label="full company name input"
             placeholder="Företagsnamn"
             {...register("companyName", { required: true, maxLength: 100 })}
           />
@@ -148,13 +152,17 @@ const CreateCompanyform = () => {
       </form>
       <div
         className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-around items-center"
+        aria-label="close the modal"
         style={showModal === true ? { display: "block" } : { display: "none" }}
         onClick={handleModal}
       >
         <div className="flex justify-center justify-items-center align-middle text-center top-1/3 relative">
           <div className="bg-white p-6 rounded  h-2/5 w-2/5 relative">
             <p>{reponsText}</p>
-            <button className="rounded w-7 absolute top-2 left-3 bg-blue-500 hover:bg-blue-700 text-white font-bold">
+            <button
+              className="rounded w-7 absolute top-2 left-3 bg-blue-500 hover:bg-blue-700 text-white font-bold"
+              aria-label="close the modal"
+            >
               X
             </button>
           </div>
