35f7ea40abacce08cf4af8634ed59cb144726e5b
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-02-07 23:24:22 +0100

    removed file

--- front/src/Components/Modal.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Modal = () => {
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center"></div>
-  );
-};
-
-export default Modal;
